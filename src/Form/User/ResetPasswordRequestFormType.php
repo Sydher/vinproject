3e34fb882d0b95f41ac50e4534a3c85d46d0b007
Sydher <hugoc.pro@gmail.com>
2020-08-09 11:36:52 +0200

    Réinitialisation du mot de passe

---
+++ src/Form/User/ResetPasswordRequestFormType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form\User;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ResetPasswordRequestFormType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
+        $builder
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your email',
+                    ]),
+                ],
+                "help" => "forgot_password_help"
+            ]);
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options) {
+        $view->vars['help'] = $options['help'] ?? '';
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void {
+        $resolver->setDefaults([]);
+    }
+
+}
